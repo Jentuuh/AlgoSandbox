826fea99bf79cd1a7265156118f4dc66292627b1
Jentuuh <jentevandersanden2009@live.be>
2020-08-29 19:31:05 +0200

    Implemented onClick event for tools

--- src/Components/Tool.jsx
+++ src/Components/Tool.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../Styling/ToolIcon.css'
 
-const Tool = ({tool, index}) => {
+const Tool = ({tool, index, clickOnTool}) => {
 
     return (
-        <img className="toolIcon" src={tool.toolIcon} alt={tool.name} />  
+        <img className="toolIcon" src={tool.toolIcon} onClick={() => clickOnTool(tool)} alt={tool.name} />  
     );
 }
  
--- src/Components/ToolBox.jsx
+++ src/Components/ToolBox.jsx
@@ -18,21 +18,22 @@ const ToolBox = () => {
     ]);
     const [equipedTool, setEquipedTool] = useState(null);
 
+    // 'onClick' event callback for when a tool is clicked. This will equip the tool.
     const equipTool = (toolToEquip) => {
         setEquipedTool(toolToEquip);
-        console.log("Tool Equiped:" + equipedTool.name);
     }
 
 
     return (
             <div className="toolbox">
                 <h2 className="h2">Tools:</h2>
+                <h3>Currently using: {equipedTool === null ? 'Nothing' : equipedTool.name}</h3>
                 {tools.map((tool, index) =>(
                         <Tool
                             key={index}
                             index={index}
                             tool={tool}
-                            onClick={(tool) => { equipTool(tool)}}
+                            clickOnTool = {equipTool}
                         />
                     ))}
             </div>);
